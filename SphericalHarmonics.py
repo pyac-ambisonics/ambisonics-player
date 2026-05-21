e5cbcd3413496f2a23eab1af6a95e986ce1cd669
sofd <kkl93@gmx.de>
2026-05-21 18:17:15 +0200

    a working example of applying spherical harmonics

--- SphericalHarmonics.py
+++ SphericalHarmonics.py
@@ -8,44 +8,42 @@ import pooch
 class SphericalHarmonics:
 
     # Constructor
-    def __init__(self, hrtf=None, ambi_file=None):
-        # this will not work yet, work out the kinks
+    def __init__(self, hrtf=None, sampling_rate=48e3, ambi_order=1):
         # # set the hrtf. this should be a pyfar signal!
         # self.hrtf = hrtf
-        # # get sampling rate from HRTF files
-        # self.sampling_rate = hrtf.sampling_rate
-        # # set the ambisonics file
-        # self.ambi_file = ambi_file
-        # # set the ambisonics order we are calcualting for
-        # self.ambi_order = ambi_file.get_ambi_order()
-        # # set up the ambisonics matrix
-        # self.ambi_matrix = self.matrix_from_ambi(ambi_file)
+
+        # set sample rate. we should make sure this is the same as for the HRTFs!!!
+        self.sample_rate = sampling_rate
 
         # load FABIAN from the web
         self.hrirs, sources = self.load_hrtf_from_web()
+
+        # make sure the sampling rate is correct and resample if necessary
+        if self.hrirs.sampling_rate != sampling_rate:
+            self.hrirs = pf.dsp.resample(self.hrirs, sampling_rate=sampling_rate, match_amplitude='freq')
+
         # store the sources in a Sampling Sphere
         self.sources = sh.SamplingSphere.from_coordinates(sources)
 
-        # create a spherical harmonics definition
-        self.n_max = 16
-        self.sh_definition = sh.SphericalHarmonicDefinition(self.n_max, normalization="SN3D")
+        # create a spherical harmonics definition, corresponding to the AmbiX convention
+        self.ambi_order = ambi_order
+        self.sh_definition = sh.SphericalHarmonicDefinition(self.ambi_order, normalization="SN3D", 
+                                                            basis_type='real', condon_shortley=False)
 
         # create the spherical harmonics object from definition and sampling sphere
         self.spherical_harmonics = sh.SphericalHarmonics.from_definition(self.sh_definition, self.sources, inverse_method="pseudo_inverse")
 
         # create h_nm matrix 
+        print("doing matrix mult to get hrirs_nm")
         hrirs_nm = (self.spherical_harmonics.basis_inv @ self.hrirs).T
+        print("convert to spherical harmonic signal")
         self.hrirs_nm = sh.SphericalHarmonicSignal.from_definition(self.sh_definition, hrirs_nm.time, hrirs_nm.sampling_rate)
 
         # prepare a rotation matrix, with all angles 0 currently
         angles = [0, 0, 0]
         self.rotation = sh.transforms.SphericalHarmonicRotation.from_euler('xyz', np.deg2rad(angles))
-
-    # does nothing as of yet
-    def matrix_from_ambi(self, ambi_file):
-        # TODO: get the shape from the ambisoncis file
-        matrix = np.zeros(shape=(ambi_file.shape))
-        return matrix
+        # calculate the rotation matrix once
+        #self.rotation_matrix = self.rotation.as_spherical_harmonic_matrix(self.sh_definition)
     
     # loads HRTFs from the internet
     def load_hrtf_from_web(self):
@@ -59,19 +57,18 @@ class SphericalHarmonics:
 
         # load HRIRs and source positions
         hrirs, sources, _ = pf.io.read_sofa(file)
+        print("Loaded HRTF from web")
         return hrirs, sources
     
     # set the current rotation angle
     def set_rotation(self, angles):
         self.rotation = sh.transforms.SphericalHarmonicRotation.from_euler('xyz', np.deg2rad(angles))
+        # recalculate the rotation matrix once
+        #self.rotation_matrix = self.rotation.as_spherical_harmonic_matrix(self.sh_definition)
 
     # apply a rotation and return them as signal
     def apply_rotation(self):
         # creates rotated HRIRs matrix
+        #hrirs_nm_rotated = self.rotation_matrix @ self.hrirs_nm
         hrirs_nm_rotated = self.rotation.apply(self.hrirs_nm)
-
-        # interpolate the HRIRs by matrix mulitplication
-        hrirs_interpolated_rotated = pf.matrix_multiplication(
-            (self.spherical_harmonics.basis, hrirs_nm_rotated), domain='time', axes=[(0, 1), (1, 0), (0, 1)])
-        hrirs_interpolated_rotated = pf.Signal(hrirs_interpolated_rotated, self.hrirs.sampling_rate)
-        return hrirs_interpolated_rotated
+        return hrirs_nm_rotated
--- Test_SH_HRTF.py
+++ Test_SH_HRTF.py
@@ -1,10 +1,76 @@
-import SphericalHarmonics
+from SphericalHarmonics import SphericalHarmonics
+import sounddevice as sd
+import soundfile as sf
+import pyfar as pf
+import numpy as np
 
 def main():
-    harmonics = SphericalHarmonics()
-    ambi_file = load_ambi_file()
+    ambi_file, ambi_order = load_ambi_file()
+    print("loaded ambi file")
+    harmonics = SphericalHarmonics(sampling_rate=ambi_file.sampling_rate, ambi_order=ambi_order)
+    print("created harmonics")
+    stereo = apply_hrtf(ambi_signal=ambi_file, sh_hrir=harmonics.apply_rotation(), sampling_rate=ambi_file.sampling_rate)
+    print("applied hrtf")
+    import time
+    start = time.time()
+    # creates rotated HRIRs matrix
+    #hrirs_nm_rotated = self.rotation_matrix @ self.hrirs_nm
+    pf.io.write_audio(stereo, "written_binaural.wav")
+    print(f"Writing took {time.time() - start:.2f} seconds")
+    #sf.write("written_binaural.wav", (left.time, right.time), int(ambi_file.sampling_rate), 'PCM_24')
+    print("wrote file")
 
 def load_ambi_file():
-    # do something
-    file = None
-    return file
+    # load audio data
+    ambi_signal = pf.io.read_audio("Ambisonics_Noise_3rd_order_noise_dir.wav")
+    print(f'{ambi_signal.cshape = }')
+    print(f'{ambi_signal.time.shape = }')
+    # we know that this file is 3rd order
+    return ambi_signal, 3
+
+# apply the hrtf data to an ambisonics file
+def apply_hrtf(ambi_signal, sh_hrir, sampling_rate):
+    # Check channel count by comparing the channel shape
+    # we know the channel shape for ambi_signal is (channels,)
+    ambi_ch, *_ = ambi_signal.cshape
+    # we know the channel shape for sh_hrir is (2, channels)
+    *_, sh_hrir_ch = sh_hrir.cshape
+    if ambi_ch != sh_hrir_ch:
+        raise ValueError("Channel counts must match (16 for 3rd order).")
+
+    # sh_hrir should have the shape (2, ambi_order)
+    # Convolve each channel separately for left and right
+    # instantly store it as time data
+    left_conv = pf.dsp.convolve(
+        ambi_signal,
+        pf.Signal(sh_hrir.time[0, :, :], sampling_rate, domain='time'), # need to get the left channel here
+        mode='full'
+    ).time
+    right_conv = pf.dsp.convolve(
+        ambi_signal,
+        pf.Signal(sh_hrir.time[1, :, :], sampling_rate, domain='time'), # need to get the right channel here
+        mode='full'
+    ).time
+    
+    # Sum over channels -> single‑channel binaural signals
+    left_signal = np.sum(left_conv, axis=0)
+    right_signal = np.sum(right_conv, axis=0)
+
+    # do gain staging
+    # Find the maximum absolute value across both channels
+    peak = max(np.abs(left_signal).max(), np.abs(right_signal).max())
+    # Avoid division by zero
+    if peak > 0:
+        gain = 0.99 / peak   # 0.99 leaves a tiny headroom
+        left_signal *= gain
+        right_signal *= gain
+
+
+    # create stereo signal by stacking the time data horizontally
+    stereo_time = np.vstack((left_signal, right_signal))
+    stereo = pf.Signal(stereo_time, sampling_rate=sampling_rate, domain='time')
+
+    return stereo
+
+if __name__ == "__main__":
+    main()
