105d8935c9c8cc6d1f8b1d1a88a15d855ab13ae0
Kylan Klein Lenderink <kkl93@gmx.de>
2026-06-01 03:55:50 +0200

    minor performance tests with spherical harmonics

--- SphericalHarmonics.py
+++ SphericalHarmonics.py
@@ -151,12 +151,14 @@ class SphericalHarmonics:
         left_conv = pf.dsp.convolve(
             ambi_signal,
             pf.Signal(sh_hrir.time[0, :, :], self.sampling_rate, domain='time'), # need to get the left channel here
-            mode='full'
+            mode='full',
+            method='overlap_add'
         ).time
         right_conv = pf.dsp.convolve(
             ambi_signal,
             pf.Signal(sh_hrir.time[1, :, :], self.sampling_rate, domain='time'), # need to get the right channel here
-            mode='full'
+            mode='full',
+            method='overlap_add'
         ).time
         print(f"Convolving signals took {time.time() - start:.4f} seconds")
 
@@ -173,7 +175,6 @@ class SphericalHarmonics:
             pre_gain = 0.99 / peak   # 0.99 leaves a tiny headroom
             left_signal *= pre_gain * gain
             right_signal *= pre_gain * gain
-
 
         # create stereo signal by stacking the time data horizontally
         stereo_time = np.vstack((left_signal, right_signal))
