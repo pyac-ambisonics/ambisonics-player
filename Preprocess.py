89f5b8a4bbeaf2f29469dbf8913f622beb5a4f26
Kylan Klein Lenderink <kkl93@gmx.de>
2026-06-03 14:54:21 +0200

    preparing to add MagLS algorithm to Preprocess

--- Preprocess.py
+++ Preprocess.py
@@ -3,7 +3,7 @@
 class Preprocess:
     
     def __init__(self):
-        self.algorithms = ['MagLS']
+        self.algorithms = ['MagLS', 'TA', 'BiMagLS']
         self.current = 'MagLS'
 
     # apply the chosen preprocessing algorithm. use MagLS as default
@@ -12,6 +12,33 @@ class Preprocess:
             case 'MagLS':
                 print("Using MagLS HRTF Preprocessing")
                 return self.__mag_ls(hrirs)
+            case 'TA':
+                print("TA not implemented yet. Using MagLS instead")
+                return self.__mag_ls(hrirs)
+            case 'BiMagLS':
+                print("BiMagLS not implemented yet. Using MagLS instead")
+                return self.__mag_ls(hrirs)
+            case _:
+                print("Using default MagLS algorithm")
+                return self.__mag_ls(hrirs)
 
-    def __mag_ls(self, hrirs):
+
+    def __mag_ls(self, hrirs, cutoff=3000):
+        # create frequency axis
+        # find cutoff frequency
+
+        # for each direction do the following steps???
+
+            # get fft of original HRTF
+
+            # for each ear:
+
+                # below cutoff: keep original
+
+                # above cutoff: magintude from original, phase from min-phase
+
+                # create time domain by ifft-ing
+
+                # apply time domain to magls_hrir for the current direction
+
         return hrirs
