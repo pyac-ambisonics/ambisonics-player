4b706744119500a0f89bd3326804e50b26ad62da
Kylan Klein Lenderink <kkl93@gmx.de>
2026-06-07 12:28:49 +0200

    continued implementation of the magls algorithm

--- HRTF_process.py
+++ HRTF_process.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pyfar as pf
 import spharpy
+from math import utils
 
 # a class making different HRTF preprocessing algorithms available
 
@@ -39,13 +40,9 @@ class HRTF_process:
         return (sh.basis_inv @ hrirs).T
 
     # apply the magnitude least squares algorithm for better results for low order ambisonics
-    def __mag_ls(self, hrirs: pf.Signal, sh: spharpy.SphericalHarmonics, cutoff=3000, ramp=0):
+    # ramp = 0 -> no ramp. ramp = 1 -> default ramp (cutoff * sqrt(2)). ramp > 1 -> specific ramp in freq
+    def __mag_ls(self, hrirs: pf.Signal, sh: spharpy.SphericalHarmonics, cutoff=3000, ramp=1):
         print("Using MagLS method.")
-        # handle some exceptions
-        if ramp < 0:
-            raise Exception("Ramp value must be >= 0")
-        if cutoff < 0:
-            raise Exception("Cutoff frequency must be >= 0")
         
         hrirs_copy = hrirs.copy()
         # make our data frequency data
@@ -58,35 +55,55 @@ class HRTF_process:
         alpha = np.zeros_like(hrirs_freq, dtype=float)
         alpha[freq_idx:] = 1
 
+        
         # make a ramp up if needed. Use Hanning for smoothness
         if ramp > 0:
-            # find start and stop indices
+            # use default ramp of sqrt(2)
+            if ramp == 1:
+                ramp = cutoff * np.sqrt(2)
+            # find start and stop indices. make sure they are not below 0 or above fs
             half = ramp / 2
             start = hrirs.find_nearest_frequency(cutoff - half)
-            start = max(start, 0)
+            # probably unnecessary, since find_nearest_freq should always return valid indices
+            # start = max(start, 0) 
             stop = hrirs.find_nearest_frequency(cutoff + half)
-            stop = min(hrirs_freq.size, stop)
+            # probably unnecessary, since find_nearest_freq should always return valid indices
+            # stop = min(hrirs_freq.size, stop)
             # length
             length = max(stop - start, 0)
             # create a Hanning window, use the half that goes from 0 -> 1
             win = np.hanning(2*length)[length:]
             alpha[start:start+length] = win
 
-        # for each direction do the following steps???
 
-            # get fft of original HRTF
-
-            # for each ear:
-
-                # below cutoff: do simple least squares
-
-                # above cutoff: do least squres magnitude from original, phase from min-phase
-                # solve the magnitude only optimization
+        # below cutoff: do simple least squares
+        # do regular  least squares -> simple matrix mult
+        hrirs_sh = (sh.basis_inv @ hrirs).T
+        nm_magls = hrirs_sh.copy()
+        Y = sh.basis.copy()
+        # for each frequency, starting at the cutoff bin
+        for f in range(start, hrirs_freq.size):
+            # above cutoff: do least squres magnitude from original, phase from min-phase
+            # solve the magnitude only optimization
+            for ear in range(2):
+                # for each ear, all directional data, and the current frequency: find magls
+                nm_magls[ear, :, f] = (
+                    alpha[f] * self.__mag_ls_iter(
+                        A=Y, 
+                        b=hrirs_freq[ear, :, f], 
+                        x_prev=nm_magls[ear, :, f-1]
+                    )
+                    + (1 - alpha[f]) * hrirs_sh[ear, :, f]
+                )
 
                 # align phase between bins
 
-            # create time domain by ifft-ing
+        # create time domain by ifft-ing
 
-            # apply time domain to magls_hrir for the current direction
+        # apply time domain to magls_hrir for the current direction
 
         return hrirs_copy
+    
+    # the core of the algorithm, this produces the actual iterative least squares evaluation
+    def __mag_ls_iter(A, b, x_prev):
+        pass
