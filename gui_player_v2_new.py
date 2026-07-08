4cc7029bf25d2d4db20c654f04f27d128fef4ce8
Kylan Klein Lenderink <kkl93@gmx.de>
2026-07-08 17:36:52 +0200

    even better volume control

--- gui_player_v2_new.py
+++ gui_player_v2_new.py
@@ -1024,7 +1024,7 @@ class AudioPlayerGUI:
             return
 
         try:
-            self.player.set_volume(volume**(1.5 + 1.5 * volume))
+            self.player.set_volume(volume**(1 + 3 * volume))
             self.update_info()
         except Exception as error:
             self._handle_error("Error", error)
